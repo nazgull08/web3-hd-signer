ece51dbfbde099b1cd415a8f2d34c69358d22f22
nazgull08 <nazgull08@protonmail.com>
2023-08-16 11:14:11 +0300

    fixing minor bugs

--- src/main.rs
+++ src/main.rs
@@ -36,7 +36,7 @@ enum Commands {
 //NTD
 // ETH gas usage 94,795 | 63,197 
 // BSC gas usage 76,654 | 51,103 
-// PLG gas usage 96,955 | 57,294
+// MTC gas usage 96,955 | 57,294
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -60,10 +60,8 @@ async fn main() {
     match args.command {
         Commands::Balance{c_from: o_c_from, c_to: o_c_to} => {
             if let Some(c_from) = o_c_from && let Some(c_to) = o_c_to{
-                println!("If In balance!!!");
                 balance(conf,c_from,c_to).await
             } else {
-                println!("Else In balance!!!");
                 balance(conf,0,10).await
             }
         },
@@ -120,12 +118,10 @@ async fn test_wallet(conf : Settings) {
         let tx_fee = g_price * 21000 * 5;
         if eth_bal_token.1 > U256::zero() {
             wal_addrs_token.push(WalletAddress { id: i, address: eth_i.clone(), balance: eth_bal.1, balance_token: (usdt.to_owned(), eth_bal_token.1) });
-        //let a_token = hdw_eth.sweep_token(i, usdt,to).await;
         }
         if eth_bal.1 > tx_fee {
             wal_addrs_eth.push(WalletAddress { id: i, address: eth_i.clone(), balance: eth_bal.1, balance_token: (usdt.to_owned(), eth_bal_token.1) });
         println!("Found {:?} money. Tx fee {tx_fee} Sweeping...",eth_bal.1);
-        //let a = hdw_eth.sweep(i, to).await;
         } else {
             println!("No funds on wallet: {:?} Skipping", eth_bal.1)
         }
@@ -177,9 +173,8 @@ async fn refill(sweeper_prvk : &str, main_addrs : Vec<WalletAddress>, token_addr
 
 
 async fn balance(conf: Settings, c_from: u32, c_to: u32) {
-    println!("in balance");
+    println!("Calcing balances...");
     let rates = rates().await;
-    println!("in balance2");
     let sweeper_prvk = conf.sweeper;
     let phrase = conf.hd_phrase;
     let hdw_eth = HDWallet::Ethereum(HDSeed::new(&phrase));
@@ -189,58 +184,66 @@ async fn balance(conf: Settings, c_from: u32, c_to: u32) {
     let mut wal_addrs_token: Vec<WalletAddress> = vec![];
 
     for i in c_from..c_to {
-        println!("i: = {:?}", i);
+        println!("---------");
         let eth_i = hdw_eth.address(i as i32);
+        println!("i: = {:?}, addr: {eth_i}", i);
         let eth_priv = hdw_eth.private(i as i32);
         let eth_pub = hdw_eth.public(i as i32);
         let eth_bal = hdw_eth.balance(i as i32).await;
         let eth_bal_token = hdw_eth.balance_token(i as i32,usdt).await;
-        println!("=======================");
-        println!("ETH");
-        println!("addr: {:?}", eth_i);
-        println!("priv: {:?}", eth_priv);
-        println!("pub: {:?}", eth_pub);
-        println!("bal: {:?}", eth_bal.1);
         let eth_bal_f = eth_bal.1.as_u128() as f64 ;
         let eth_bal_f_prep = eth_bal_f / 1_000_000_000_000_000_000.0;
-        let eth_bal_in_usd = eth_bal_f_prep / rates.ETH;
-        println!("bal_f: {:.15}", eth_bal_f_prep);
-        println!("bal_in_usd: {:.15}", eth_bal_in_usd);
-        println!("bal_token: {:?}", eth_bal_token.1);
-        println!("=======================");
+        let eth_bal_in_usd = eth_bal_f_prep * rates.eth;
         let g_price = gas_price().await.unwrap(); 
-        let tx_fee = g_price * 21000 * 5;
+        let tx_fee: U256 = g_price * 21000 * 5;
+        let tx_fee_prep = tx_fee.as_u128() as f64 / 1_000_000_000_000_000_000.0;
         if eth_bal_token.1 > U256::zero() {
             wal_addrs_token.push(WalletAddress { id: i, address: eth_i.clone(), balance: eth_bal.1, balance_token: (usdt.to_owned(), eth_bal_token.1) });
-        //let a_token = hdw_eth.sweep_token(i, usdt,to).await;
+            println!("Found {:.10} token money.",eth_bal_f_prep);
+            println!("bal: {:?}", eth_bal.1);
+            println!("bal_in_usd: {:.15}", eth_bal_in_usd);
+            println!("bal_token: {:?}", eth_bal_token.1);
         }
         if eth_bal.1 > tx_fee {
             wal_addrs_eth.push(WalletAddress { id: i, address: eth_i.clone(), balance: eth_bal.1, balance_token: (usdt.to_owned(), eth_bal_token.1) });
-        println!("Found {:?} money. Tx fee {tx_fee}",eth_bal.1);
-        //let a = hdw_eth.sweep(i, to).await;
+            println!("Found {:.10} main money. Tx fee {tx_fee_prep}",eth_bal_f_prep);
+            println!("bal: {:?}", eth_bal.1);
+            println!("bal_in_usd: {:.15}", eth_bal_in_usd);
+            println!("bal_token: {:?}", eth_bal_token.1);
         } else {
-            println!("No funds on wallet: {:?} Skipping", eth_bal.1)
+            println!("No funds on wallet. Skipping")
         }
 
     }
 }
 #[allow(non_snake_case)]
 #[derive(Debug,Deserialize)]
-pub struct Rates{
+pub struct RatesRaw{
     pub ETH: f64,
     pub TRX: f64,
-    pub PLG: f64,
+    pub MATIC: f64,
     pub BNB: f64,
     pub XLM: f64,
 }
 
+#[derive(Debug,Deserialize)]
+pub struct Rates{
+    pub eth: f64,
+    pub trx: f64,
+    pub mtc: f64,
+    pub bnb: f64,
+    pub xlm: f64,
+}
+
 async fn rates() -> Rates {
     println!("getting current rates...");
-    let rs:Rates = reqwest::get("https://min-api.cryptocompare.com/data/price?fsym=USD&tsyms=ETH,TRX,PLG,BNB,XLM")
+    let rs:RatesRaw = reqwest::get("https://min-api.cryptocompare.com/data/price?fsym=USD&tsyms=ETH,TRX,MATIC,BNB,XLM")
         .await
         .unwrap()
-        .json::<Rates>()
+        .json::<RatesRaw>()
         .await
         .unwrap();
-    rs
+    let rates: Rates = Rates { eth: 1./rs.ETH, trx: 1./rs.TRX, mtc: 1./rs.MATIC, bnb: 1./rs.BNB, xlm: 1./rs.XLM};
+    println!("Rates:\nETH:{:.4}$\nTRX:{:.4}$\nMTC:{:.4}$\nBNB:{:.4}$\nXLM:{:.4}$\n", rates.eth,rates.trx,rates.mtc,rates.bnb,rates.xlm);
+    rates
 }
